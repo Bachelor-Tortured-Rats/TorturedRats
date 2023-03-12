64d74a8fcbcff8ce4f69a8b151e716376f417911
christian-kento-rasmussen <christian.kento@gmail.com>
2023-03-12 10:34:11 +0100

    updated logging to use logging and wandb

--- src/models/unet_model.py
+++ src/models/unet_model.py
@@ -2,13 +2,14 @@ from monai.networks.nets import UNet
 import torch
 from monai.networks.layers import Norm
 
+
 def load_unet(model_path: str, device,
-              spatial_dims  = 3,
-              in_channels   = 1,
-              out_channels  = 2,
-              channels      = (16, 32, 64, 128, 256),
-              strides       = (2, 2, 2, 2),
-              num_res_units = 2,
+              spatial_dims=3,
+              in_channels=1,
+              out_channels=2,
+              channels=(16, 32, 64, 128, 256),
+              strides=(2, 2, 2, 2),
+              num_res_units=2,
               ):
     """Loads a trained unet model
 
@@ -19,27 +20,29 @@ def load_unet(model_path: str, device,
         model: a loaded pytorch unet model
     """
     checkpoint = torch.load(model_path, map_location=device)
+    params = {
+        "spatial_dims": checkpoint['spatial_dims'],
+        "in_channels": checkpoint['in_channels'],
+        "out_channels": checkpoint['out_channels'],
+        "channels": checkpoint['channels'],
+        "strides": checkpoint['strides'],
+        "num_res_units": checkpoint['num_res_units'],
+    }
 
-    model = create_unet(device=device,
-        spatial_dims    = checkpoint['spatial_dims'],
-        in_channels     = checkpoint['in_channels'],
-        out_channels    = checkpoint['out_channels'],
-        channels        = checkpoint['channels'],
-        strides         = checkpoint['strides'],
-        num_res_units   = checkpoint['num_res_units']
-    )
-
+    model = create_unet(device=device, **params)
     model.load_state_dict(checkpoint['model_state_dict'])
-    return model
+    
+    return model, params
+
 
 def create_unet(device,
-              spatial_dims  = 3,
-              in_channels   = 1,
-              out_channels  = 2,
-              channels      = (16, 32, 64, 128, 256),
-              strides       = (2, 2, 2, 2),
-              num_res_units = 2,
-              ):
+                spatial_dims=3,
+                in_channels=1,
+                out_channels=2,
+                channels=(16, 32, 64, 128, 256),
+                strides=(2, 2, 2, 2),
+                num_res_units=2,
+                ):
     """Loads a trained unet model
 
     Args:
@@ -48,15 +51,15 @@ def create_unet(device,
     Returns:
         model: a loaded pytorch unet model
     """
-    model = UNet(
-        spatial_dims    = spatial_dims,
-        in_channels     = in_channels,
-        out_channels    = out_channels,
-        channels        = channels,
-        strides         = strides,
-        num_res_units   = num_res_units,
-        norm            = Norm.BATCH,
-    )
+    params = {
+        "spatial_dims": spatial_dims,
+        "in_channels": in_channels,
+        "out_channels": out_channels,
+        "channels": channels,
+        "strides": strides,
+        "num_res_units": num_res_units,
+    }
+    model = UNet(norm=Norm.BATCH, **params)
     model.to(device)
 
-    return model
+    return model, params
--- src/pipelines/train_model.py
+++ src/pipelines/train_model.py
@@ -10,7 +10,6 @@ from monai.transforms import (
     ScaleIntensityRanged,
     Spacingd,
 )
-
 from monai.networks.nets import UNet
 from monai.networks.layers import Norm
 from monai.metrics import DiceMetric
@@ -20,6 +19,8 @@ from monai.data import CacheDataset, DataLoader, decollate_batch
 import torch
 import matplotlib.pyplot as plt
 import click
+import wandb
+import logging
 
 
 from src.models.unet_model import create_unet, load_unet
@@ -27,10 +28,14 @@ from src.data.IRCAD_dataset import load_IRCAD_dataset
 from src.data.hepatic_dataset import load_hepatic_dataset
 
 
-def train_model(model, device, train_loader, val_loader, max_epochs, lr, data_type, pt,model_save_path):
+def train_model(model, device, train_loader, val_loader, max_epochs, lr, data_type, pt, model_save_path, wandb_logging):
     loss_function = DiceLoss(to_onehot_y=True, softmax=True)
     optimizer = torch.optim.Adam(model.parameters(), lr)
     dice_metric = DiceMetric(include_background=False, reduction="mean")
+
+    # logging setup
+    wandb.watch(model, criterion=loss_function, log="all", log_freq=2)
+    logger = logging.getLogger(__name__)
 
     max_epochs = max_epochs
     val_interval = 2
@@ -42,8 +47,8 @@ def train_model(model, device, train_loader, val_loader, max_epochs, lr, data_ty
     post_label = Compose([AsDiscrete(to_onehot=2)])
 
     for epoch in range(max_epochs):
-        print("-" * 10)
-        print(f"epoch {epoch + 1}/{max_epochs}")
+        logger.info("-" * 10)
+        logger.info(f"epoch {epoch + 1}/{max_epochs}")
         model.train()
         epoch_loss = 0
         step = 0
@@ -53,7 +58,7 @@ def train_model(model, device, train_loader, val_loader, max_epochs, lr, data_ty
                 batch_data["image"].to(device),
                 batch_data["label"].to(device),
             )
-            
+
             optimizer.zero_grad()
             outputs = model(inputs)
             loss = loss_function(outputs, labels)
@@ -63,7 +68,6 @@ def train_model(model, device, train_loader, val_loader, max_epochs, lr, data_ty
 
         epoch_loss /= step
         epoch_loss_values.append(epoch_loss)
-        print(f"epoch {epoch + 1} average loss: {epoch_loss:.4f}")
 
         if (epoch + 1) % val_interval == 0:
             model.eval()
@@ -76,9 +80,12 @@ def train_model(model, device, train_loader, val_loader, max_epochs, lr, data_ty
 
                     roi_size = (160, 160, 160)
                     sw_batch_size = 4
-                    val_outputs = sliding_window_inference(val_inputs, roi_size, sw_batch_size, model)
-                    val_outputs = [post_pred(i) for i in decollate_batch(val_outputs)]
-                    val_labels = [post_label(i) for i in decollate_batch(val_labels)]
+                    val_outputs = sliding_window_inference(
+                        val_inputs, roi_size, sw_batch_size, model)
+                    val_outputs = [post_pred(i)
+                                   for i in decollate_batch(val_outputs)]
+                    val_labels = [post_label(i)
+                                  for i in decollate_batch(val_labels)]
                     # compute metric for current iteration
                     dice_metric(y_pred=val_outputs, y=val_labels)
 
@@ -92,33 +99,38 @@ def train_model(model, device, train_loader, val_loader, max_epochs, lr, data_ty
                     best_metric = metric
                     best_metric_epoch = epoch + 1
                     torch.save({
-                            'model_state_dict'  : model.state_dict(),
-                            'spatial_dims'      : model.dimensions,
-                            'in_channels'       : model.in_channels,
-                            'out_channels'      : model.out_channels,
-                            'channels'          : model.channels,
-                            'strides'           : model.strides,
-                            'num_res_units'     : model.num_res_units,
-                            'epoch'             : best_metric_epoch,
-                            'best_metric'       : best_metric,
-                            }, "{folder_path}/{data_type}_{pt}_e{max_epochs}_lr{lr:.0E}_bmm.pth".format(
-                                folder_path     = model_save_path,
-                                data_type       = data_type, 
-                                pt              = pt,
-                                max_epochs      = max_epochs, 
-                                lr              = lr
-                                )
-                            )
-                    print("saved new best metric model")
-                print(
+                        'model_state_dict': model.state_dict(),
+                        'spatial_dims': model.dimensions,
+                        'in_channels': model.in_channels,
+                        'out_channels': model.out_channels,
+                        'channels': model.channels,
+                        'strides': model.strides,
+                        'num_res_units': model.num_res_units,
+                        'epoch': best_metric_epoch,
+                        'best_metric': best_metric,
+                    }, "{folder_path}/{data_type}_{pt}_e{max_epochs}_lr{lr:.0E}_bmm.pth".format(
+                        folder_path=model_save_path,
+                        data_type=data_type,
+                        pt=pt,
+                        max_epochs=max_epochs,
+                        lr=lr
+                    )
+                    )
+                    logger.info("saved new best metric model")
+
+                wandb.log(step=epoch, data={
+                          "mean dice": metric, "train_loss": epoch_loss})
+                logger.info(
                     f"current epoch: {epoch + 1} current mean dice: {metric:.4f}"
                     f"\nbest mean dice: {best_metric:.4f} "
                     f"at epoch: {best_metric_epoch}"
                 )
-    return model, best_metric ,best_metric_epoch, epoch_loss_values, val_interval, metric_values
+    return model, best_metric, best_metric_epoch, epoch_loss_values, val_interval, metric_values
 
-def display_model_training(best_metric ,best_metric_epoch, epoch_loss_values, val_interval, metric_values,figures_save_path):
-    print(f"train completed, best_metric: {best_metric:.4f} " f"at epoch: {best_metric_epoch}")
+
+def display_model_training(best_metric, best_metric_epoch, epoch_loss_values, val_interval, metric_values, figures_save_path):
+    print(
+        f"train completed, best_metric: {best_metric:.4f} " f"at epoch: {best_metric_epoch}")
 
     plt.figure("train", (12, 6))
     plt.subplot(1, 2, 1)
@@ -135,6 +147,7 @@ def display_model_training(best_metric ,best_metric_epoch, epoch_loss_values, va
     plt.plot(x, y)
     plt.savefig(f'{figures_save_path}/training_graph.png')
 
+
 @click.command()
 @click.option('--data_type', '-d', type=click.Choice(['IRCAD', 'hepatic'], case_sensitive=False), default='IRCAD', help='Dataset choice, defaults to IRCAD')
 # @click.argument('data_path', type=click.Path(exists=True), default='/work3/s204159/3Dircadb1/')
@@ -143,28 +156,63 @@ def display_model_training(best_metric ,best_metric_epoch, epoch_loss_values, va
 @click.option('--lr', '-lr', type=click.FLOAT, default=1e-4, help='Learning rate, defaults to 1e-4')
 @click.option('--model_save_path', type=click.Path(exists=True), default='models', help='Path to folder for saving model')
 @click.option('--figures_save_path', type=click.Path(exists=True), default='reports/figures/train_model', help='Path to folder for saving figures')
-def main(data_type, pretrained, epochs, lr, model_save_path, figures_save_path):
+@click.option('--wandb_logging', '-l', click.Choice(['online', 'offline', 'disabled']), default="disabled", help='Should wandb logging be enabled: Can be "online", "offline" or "disabled"')
+def main(data_type, pretrained, epochs, lr, model_save_path, figures_save_path, wandb_logging):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    # initializes logging
+    log_fmt = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    logging.basicConfig(level=logging.INFO, format=log_fmt)
+    logger = logging.getLogger(__name__)
+    logger.info('Initialized logging')
+    logger.info(f'Using dataset {data_type}')
+
+    # initializing wandb
+    config = {
+        "epochs": epochs,
+        "learning_rate": lr,
+        "pretrained": pretrained,
+        "dataset": data_type
+    }
+
+    run = wandb.init(
+        project="TorturedRats",
+        notes="test run",
+        tags=["testing"],
+        config=config,
+        mode=wandb_logging,
+    )
 
     if data_type == 'IRCAD':
         data_path = '/work3/s204159/3Dircadb1/'
-        train_loader, val_loader = load_IRCAD_dataset(data_path)#, train_patients=[],val_patients=[1,4])
+        # , train_patients=[],val_patients=[1,4])
+        train_loader, val_loader = load_IRCAD_dataset(data_path)
     elif data_type == 'hepatic':
         data_path = '/dtu/3d-imaging-center/courses/02510/data/MSD/Task08_HepaticVessel/'
         train_loader, val_loader = load_hepatic_dataset(data_path)
-    
+
     if pretrained != '':
-        model = load_unet(pretrained, device=device)
+        model, params = load_unet(pretrained, device=device)
         pt = 'finetuned'
     else:
-        model = create_unet(device=device)
+        model, params = create_unet(device=device)
         pt = 'standard'
 
-    model, best_metric, best_metric_epoch, epoch_loss_values, val_interval, metric_values = train_model(model, device, train_loader, val_loader, max_epochs=epochs, lr=lr, data_type=data_type, pt=pt,model_save_path=model_save_path)
-    
-    display_model_training(best_metric, best_metric_epoch, epoch_loss_values, val_interval, metric_values,figures_save_path)
+    if wandb_logging:
+        wandb.config.update(params)
+    else:
+        logger.info(f'using model with params: {params}')
+
+    model, best_metric, best_metric_epoch, epoch_loss_values, val_interval, metric_values = train_model(
+        model, device, train_loader, val_loader, max_epochs=epochs, lr=lr, data_type=data_type, pt=pt, model_save_path=model_save_path)
+
+    display_model_training(best_metric, best_metric_epoch, epoch_loss_values,
+                           val_interval, metric_values, figures_save_path)
+
+    wandb.finish()
+
 
 if __name__ == "__main__":
     set_determinism(seed=420)
-    
+
     main()
