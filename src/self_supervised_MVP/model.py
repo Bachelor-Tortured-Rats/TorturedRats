9752bfa91ba5a23e7370f20b3ad29fe2e76606d4
eigilalbaek@gmail.com <eigilalbaek@gmail.com>
2023-03-17 14:26:13 +0100

    Simpel auto encoder der virker

--- src/self_supervised_MVP/model.py
+++ src/self_supervised_MVP/model.py
@@ -2,63 +2,68 @@ import torch
 import torch.nn as nn
 from Make_patches import *
 
-
 class Autoencoder(nn.Module):
     def __init__(self):
         super(Autoencoder, self).__init__()
-        # Encoder
+
+        # Encoder layers
         self.encoder = nn.Sequential(
-            nn.Conv3d(1, 16, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool3d(kernel_size=2, stride=2),
-            nn.Conv3d(16, 32, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool3d(kernel_size=2, stride=2),
-            nn.Conv3d(32, 64, kernel_size=3, stride=1, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool3d(kernel_size=2, stride=2),
+            nn.Conv2d(1, 16, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 8, kernel_size=3, stride=2, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(8, 4, kernel_size=3, stride=2, padding=1),
+            nn.ReLU()
         )
 
-        """
-        # Decoder
+        
+        # Decoder layers
         self.decoder = nn.Sequential(
-            nn.ConvTranspose3d(64, 32, kernel_size=2, stride=2),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose3d(32, 16, kernel_size=2, stride=2),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose3d(16, 1, kernel_size=2, stride=2),
-            nn.Sigmoid(),
+            nn.ConvTranspose2d(4, 8, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(8, 16, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.ReLU(),
+            nn.ConvTranspose2d(16, 1, kernel_size=3, stride=2, padding=1, output_padding=1),
+            nn.Sigmoid()
         )
-        """
-    
+        
+
     def forward(self, x):
-        # Encode
+        # Encoder
         x = self.encoder(x)
-        # Decode
-        #x = self.decoder(x)
+
+        
+        # Decoder
+        x = self.decoder(x)
+
         return x
 
 
 if __name__ == "__main__":
     # Load an example scan
     exampleImage = nib.load("/dtu/3d-imaging-center/courses/02510/data/MSD/Task08_HepaticVessel/imagesTr/hepaticvessel_458.nii.gz")
-    image = exampleImage.get_fdata()
+    image = exampleImage.get_fdata()[:,:,100]
 
     # Create an instance of the model
     autoencoder = Autoencoder()
 
     # Create some input data (e.g., a single-channel 3D image with dimensions 8x8x8)
     #x = torch.randn(1, 1, 8, 8, 8)
-    x = extract_patches(image, patch_size=(20,20,20))
-    x = torch.from_numpy(np.expand_dims(x[(15, 15, 5)], axis=(0,1))).double()
+    #x = extract_patches(image, patch_size=(20,20,20))
+    x = torch.from_numpy(np.expand_dims(image, axis=(0,1))).float() #.double()
+
+    print(x.shape)
 
     # Pass the input through the model to obtain the reconstructed output
     output = autoencoder(x)
 
     # Compute the reconstruction loss (e.g., using mean squared error)
-    #loss_fn = nn.MSELoss()
-    #loss = loss_fn(output, x)
+    loss_fn = nn.MSELoss()
+    loss = loss_fn(output, x)
+    
+    
+
 
     #print(loss)
-    print(output)
-    print(output.shape)
+    #print(output)
+    #print(output.shape)
