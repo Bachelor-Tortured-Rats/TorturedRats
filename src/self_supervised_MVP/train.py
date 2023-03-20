aa5fd1da67961f31e208a8edacb23acf7d9ed8b9
eigilalbaek@gmail.com <eigilalbaek@gmail.com>
2023-03-20 14:41:07 +0100

    Training loop kører men er ikke debugged

--- src/self_supervised_MVP/train.py
+++ src/self_supervised_MVP/train.py
@@ -3,10 +3,13 @@ from torch import nn, optim
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 
-from src.self_supervised.model import Encoder, Pred_head
+from model import Encoder, Pred_head
 
 import torch
 import torch.nn.functional as F
+
+from retinalVesselDataset import RetinalVesselDataset, RetinalVessel_collate_fn
+
 
 def train_loop(encoder, prediction_head, train_loader, optimizer, device):
     # Set the model to training mode
@@ -21,6 +24,11 @@ def train_loop(encoder, prediction_head, train_loader, optimizer, device):
         data_patch_center = data_patch_center.to(device)
         data_patch_offset = data_patch_offset.to(device)
         labels = labels.to(device)
+
+        # Ensure that all data is of type float32
+        data_patch_center = data_patch_center.float()
+        data_patch_offset = data_patch_offset.float()
+        labels = labels.float()
         
         # Zero the gradients
         optimizer.zero_grad()
@@ -34,10 +42,14 @@ def train_loop(encoder, prediction_head, train_loader, optimizer, device):
         
         # Compute the loss based on the relative location of the patches
         loss = F.binary_cross_entropy_with_logits(prediction.squeeze(), labels)
-        
+
         # Backward pass and optimization step
         loss.backward()
         optimizer.step()
+        
+        # Save the encoder state dict
+        encoder_state_dict = encoder.state_dict()
+        torch.save(encoder_state_dict, 'encoder_state_dict.pth')
         
     # Return the average loss over the training data
     return loss.item()
@@ -45,42 +57,25 @@ def train_loop(encoder, prediction_head, train_loader, optimizer, device):
 
 if __name__ == "__main__":
 
-    # Load the CIFAR-10 dataset
-    #train_dataset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
-    #train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+    encoder = Encoder()
+    prediction_head = Pred_head()
 
-    # A helper function that take an index and a length and returns a one-hot vector
-    def one_hot(index, length):
-        vector = torch.zeros(length)
-        vector[index] = 1
-        return vector
+    dataset = RetinalVesselDataset()
 
-    # Make a custom dataset for finding bugs
-    class CustomDataset(torch.utils.data.Dataset):
-        def __init__(self, data, labels):
-            self.data = torch.randn(100, 2, 40, 40)
-            self.labels = torch.randn(1, 8)
+    train_loader = DataLoader(dataset, batch_size=4, shuffle=True, collate_fn=RetinalVessel_collate_fn)
 
-        def __getitem__(self, index):
-            return self.data[index], self.labels[index]
-
-        def __len__(self):
-            return len(self.data)
-
-    # Make a random 8d one-hot vector
-
-
+    
     # Set the device to use for training
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     # Set the models and optimizer to use the device
     encoder.to(device)
     prediction_head.to(device)
-    optimizer = optim.Adam(list(encoder.parameters()) + list(prediction_head.parameters()), lr=0.001)
+    optimizer = optim.Adam(list(encoder.parameters()) + list(prediction_head.parameters()), lr=0.0001)
 
     # Train the models using the custom loss function and the train loop
-    for epoch in range(10):
-        train_loss = train_loop(encoder, prediction_head, train_loader, optimizer, device, loss_fn=patch_location_loss)
+    for epoch in range(1000):
+        train_loss = train_loop(encoder, prediction_head, train_loader, optimizer, device)
         print(f"Epoch {epoch+1}, Loss: {train_loss:.4f}")
 
     
